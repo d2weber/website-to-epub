aad843eab4d8fa02d12573d74362f4242dba953d
d2weber <29163905+d2weber@users.noreply.github.com>
2022-07-16 19:07:42 +0200

    Replace pandoc with gen-epub

--- index.ts
+++ index.ts
@@ -5,13 +5,14 @@ import path from "path";
 const PORT = process.env.PORT || 5000;
 import { Readability } from "@mozilla/readability";
 import { JSDOM } from "jsdom";
+const Epub = require("epub-gen");
 
 const article_for = async function (url: string) {
   try {
     const { document } = (await JSDOM.fromURL(url)).window;
     const article = new Readability(document).parse();
     if (!article) throw Error("Problem with `readability`");
-    return "<h1>" + article.title + "</h1>" + article.content;
+    return article;
   } catch (e: unknown) {
     if (e instanceof Error)
       throw Error(
@@ -22,46 +23,41 @@ const article_for = async function (url: string) {
 };
 
 async function epubHandler(req: Request, res: Response) {
-  let url_list = req.query.src_url;
-  if (typeof url_list !== "string") {
+  const url = req.query.src_url;
+  if (typeof url !== "string") {
     res.render("pages/index", { error: Error("Invalid URL provided.") });
     return;
   }
+  // TODO: Fix processing of multiple urls in list
   // if (!Array.isArray(url_list)) {
-  url_list = [url_list];
+  // url_list = [url_list];
   // }
   try {
-    const stdout = execFileSync(
-      "pandoc",
-      [
-        "-f",
-        "html",
-        "-t",
-        "epub",
-        "--template",
-        "./default.epub3",
-        "--abbreviations",
-        "./abbreviations",
-        "--css",
-        "./epub.css",
-        "-o",
-        "-",
-      ],
-      // TODO: sanative input
-      // TODO: Fix processing of multiple urls in list
-      {
-        input: prefix + (await Promise.all(url_list.map(article_for))).join(""),
-      }
-    );
-    res.attachment("reading_list.epub");
-    res.send(stdout).end();
+    const article = await article_for(url);
+
+    // todo: avoid race conditions on file
+    await new Epub({
+      title: "hello",
+      content: [{ title: article.title, data: article.content }],
+      output: filename,
+    }).promise
+      .then(
+        () => console.log("Ebook Generated Successfully!"),
+        (err: any) => {
+          console.error("Failed to generate Ebook because of ", err);
+        }
+      )
+      .then(() =>
+        res.sendFile(filename, (err) => {
+          if (err != undefined) throw err;
+        })
+      );
   } catch (error) {
     res.render("pages/index", { error: error });
   }
 }
 
-const prefix =
-  "<html><head><meta charset='utf-8'><title>Reading list</title></head>";
+const filename = "/tmp/reading_list.epub";
 
 express()
   .use(express.static(path.join(__dirname, "public")))
